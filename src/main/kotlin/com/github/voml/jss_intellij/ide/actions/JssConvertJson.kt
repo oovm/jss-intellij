e4d8ed11004889b2e8a576ee71b8b2ec9b69fa67
Aster <galaster@foxmail.com>
2021-09-22 12:42:23 +0800

    🔮 Create jss from json schema

--- src/main/kotlin/com/github/voml/jss_intellij/ide/actions/JssConvertJson.kt
+++ src/main/kotlin/com/github/voml/jss_intellij/ide/actions/JssConvertJson.kt
@@ -8,59 +8,61 @@ import com.intellij.json.psi.JsonFile
 import com.intellij.json.psi.JsonObject
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.LangDataKeys
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.psi.*
+import com.intellij.openapi.application.WriteAction
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
 
-
-
-class JssConvertJson : DumbAwareAction(name, description, JssIcons.FILE) {
+class JssConvertJson : CreateFileAction(name, description, JssIcons.FILE) {
     companion object {
         private val name = JssBundle.message("action.convert_json")
         private val description = JssBundle.message("action.convert_json.description")
 
-        fun tryGetJsonSchema(file: PsiFile): JsonObject? {
-            if (file is JsonFile) {
-                when (val root = file.topLevelValue) {
-                    is JsonObject -> {
-                        if (root.findProperty("\$schema") != null) {
-                            return root
-                        }
-                    }
-                }
-            }
-            return null
-        }
     }
+
+    private var sourceFile: PsiFile? = null;
 
     override fun update(event: AnActionEvent) {
+        sourceFile = LangDataKeys.PSI_FILE.getData(event.dataContext)
         super.update(event)
-//        if (ActionPlaces.isPopupPlace(event.place)) {
-//            val presentation = event.presentation
-//            presentation.isVisible = presentation.isEnabled
-//        }
     }
 
 
-    override fun actionPerformed(e: AnActionEvent) {
-        val file = LangDataKeys.PSI_FILE.getData(e.dataContext)
-        val document = file?.let { tryGetJsonSchema(it) } ?: return;
-        val buffer = StringBuilder()
-        buffer.append(
-            """${document.propertyList}
-"""
-        )
 
-        var name: String = file.name;
-        if (name.endsWith(".json")) {
-            name = name.substring(0, file.name.length - 5)
+    override fun create(newName: String, directory: PsiDirectory?): Array<PsiElement> {
+        val mkdirs = MkDirs(newName, directory!!)
+        val array = when (sourceFile) {
+            is JsonFile -> createFromJson(sourceFile as JsonFile, newName)
+            else -> null
         }
-        name = "$name.jss"
-        val fileVirtual =
-            PsiFileFactory.getInstance(file.project).createFileFromText(name, JssFileType.INSTANCE, buffer)
-        val doc = PsiDocumentManager.getInstance(fileVirtual.project).getDocument(fileVirtual)!!
-        FileDocumentManager.getInstance().saveDocument(doc)
-
+        mkdirs.directory.add(array!!.originalElement)
+        return arrayOf(WriteAction.compute<PsiFile, RuntimeException> {
+            array
+        })
     }
 }
 
+
+fun createFromJson(source: JsonFile, name: String): PsiFile? {
+    val document = tryGetJsonSchema(source) ?: return null;
+    val buffer = StringBuilder()
+    buffer.append(
+        """${document.propertyList}
+"""
+    )
+    return PsiFileFactory.getInstance(source.project).createFileFromText(name, JssFileType.INSTANCE, buffer)
+}
+
+fun tryGetJsonSchema(file: PsiFile): JsonObject? {
+    if (file is JsonFile) {
+        when (val root = file.topLevelValue) {
+            is JsonObject -> {
+                if (root.findProperty("\$schema") != null) {
+                    return root
+                }
+            }
+        }
+    }
+    return null
+}
