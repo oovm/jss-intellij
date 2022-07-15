dc0b13a2b50e9fd4333819f51fa0f1cdd5839462
Aster <galaster@foxmail.com>
2022-07-15 21:39:48 +0800

    Get type name

--- src/main/kotlin/jss/intellij/language/file/JssIcons.kt
+++ src/main/kotlin/jss/intellij/language/file/JssIcons.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader
 
 object JssIcons {
     val FILE = IconLoader.getIcon("/icons/jssIcon.svg", JssIcons::class.java)
-    val SCHEMA = AllIcons.Actions.DependencyAnalyzer
+    val SCHEMA = AllIcons.Actions.Annotate
     val IDIOM = AllIcons.Nodes.Gvariable
     val PROPERTY = AllIcons.Nodes.Property
     val ANNOTATION = AllIcons.Nodes.ObjectTypeAttribute
